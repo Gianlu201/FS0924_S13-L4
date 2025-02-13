7c317c396bd6b4ca7a995f807aca45600579a2da
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-02-13 15:35:37 +0100

    Aggiungere i file di progetto.

---
+++ FS0924_S13-L4/Models/User.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FS0924_S13_L4.Models
+{
+    public static class User
+    {
+        private static string Username { get; set; }
+        private static string Password { get; set; }
+        private static bool AccessState { get; set; } = false;
+        public static List<DateTime> AccessHistory { get; set; } = new List<DateTime>();
+
+        public static void SetUserInfo(string username, string password, DateTime date)
+        {
+            Username = username;
+            Password = password;
+            AccessHistory.Add(date);
+            AccessState = true;
+        }
+
+        public static string GetUsername()
+        {
+            return Username;
+        }
+
+        public static bool Login(string password, DateTime now)
+        {
+            if (password == Password)
+            {
+                AccessState = true;
+                AccessHistory.Add(now);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool Logout()
+        {
+            if (AccessState)
+            {
+                AccessState = false;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsUserLogged()
+        {
+            if (AccessState)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ FS0924_S13-L4/Program.cs
@@ -0,0 +1,112 @@
+﻿using FS0924_S13_L4.Models;
+
+bool loop = true;
+
+while (loop)
+{
+    Console.Clear();
+    Console.WriteLine("==========OPERAZIONI==========");
+    Console.WriteLine("Choose an option:");
+    Console.WriteLine("1.: Login");
+    Console.WriteLine("2.: Logout");
+    Console.WriteLine("3.: Verify last login");
+    Console.WriteLine("4.: Show all logins");
+    Console.WriteLine("5.: Exit");
+
+    string choice = Console.ReadLine();
+    if (int.TryParse(choice, out int value))
+    {
+        switch (value)
+        {
+            case 1:
+                Console.Clear();
+                Console.Write("Username: ");
+                string username = Console.ReadLine();
+
+                Console.Write("Password: ");
+                string password = Console.ReadLine();
+                Console.Write("Repeat password: ");
+                string confirmPassword = Console.ReadLine();
+
+                if (password == confirmPassword)
+                {
+                    if (User.GetUsername() == null)
+                    {
+                        User.SetUserInfo(username, password, DateTime.Now);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Welcome {User.GetUsername()}!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (User.GetUsername() == username && User.IsUserLogged())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Welcome {username}, you'r just logged!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else if (User.GetUsername() == username && !User.IsUserLogged())
+                    {
+                        if (User.Login(password, DateTime.Now))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine(
+                                $"Welcome {User.GetUsername()}! Nice to met you again!"
+                            );
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("ERROR! Check your password!");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR! Passwords don't match! Press a key to continue..");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.ReadKey();
+                }
+
+                Console.ReadKey();
+                break;
+            case 2:
+                if (User.Logout())
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Bye bye, see you soon!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You'r not logged!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                Console.ReadKey();
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                loop = false;
+                break;
+            default:
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR! Invalid option! Press a key to continue..");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadKey();
+                break;
+        }
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("ERROR! Invalid option! Press a key to continue..");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.ReadKey();
+    }
+}
